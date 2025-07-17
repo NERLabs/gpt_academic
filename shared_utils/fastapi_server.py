a3b397605f021a9f996aa137e3ae63b5cd15df47
刘十五 <shiwenlongyes@163.com>
2025-07-17 17:19:51 +0800

    refactor(fastapi_server): 注释掉 EdgeTTS 相关代码
    - 注释掉了使用 EdgeTTS 进行文本转语音的核心代码
    - 保留了与 EdgeTTS 相关的配置和变量定义
    - 此修改可能是为了暂时禁用 EdgeTTS 功能或进行调试

--- shared_utils/fastapi_server.py
+++ shared_utils/fastapi_server.py
@@ -213,19 +213,19 @@ def start_app(app_block, CONCURRENT_COUNT, AUTHENTICATION, PORT, SSL_KEYFILE, SS
                         from pydub import AudioSegment
                         json = await request.json()
                         voice = get_conf("EDGE_TTS_VOICE")
-                        tts = edge_tts.Communicate(text=json['text'], voice=voice)
+                        # tts = edge_tts.Communicate(text=json['text'], voice=voice)
                         temp_folder = tempfile.gettempdir()
                         temp_file_name = str(uuid.uuid4().hex)
                         temp_file = os.path.join(temp_folder, f'{temp_file_name}.mp3')
-                        await tts.save(temp_file)
-                        try:
-                            # mp3_audio = AudioSegment.from_file(temp_file, format="mp3")
-                            # mp3_audio.export(temp_file, format="wav")
-                            # with open(temp_file, 'rb') as wav_file: t = wav_file.read()
-                            # os.remove(temp_file)
-                            return Response(content=t)
-                        except:
-                            raise RuntimeError("ffmpeg未安装，无法处理EdgeTTS音频。安装方法见`https://github.com/jiaaro/pydub#getting-ffmpeg-set-up`")
+                        # await tts.save(temp_file)
+                        # try:
+                        #     # mp3_audio = AudioSegment.from_file(temp_file, format="mp3")
+                        #     # mp3_audio.export(temp_file, format="wav")
+                        #     # with open(temp_file, 'rb') as wav_file: t = wav_file.read()
+                        #     # os.remove(temp_file)
+                        #     return Response(content=t)
+                        # except:
+                        #     raise RuntimeError("ffmpeg未安装，无法处理EdgeTTS音频。安装方法见`https://github.com/jiaaro/pydub#getting-ffmpeg-set-up`")
                     if TTS_TYPE == "LOCAL_SOVITS_API":
                         # Forward the request to the target service
                         TARGET_URL = get_conf("GPT_SOVITS_URL")
